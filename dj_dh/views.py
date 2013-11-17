fbbf491c58f218538acf6e33ce22186fa601007e
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-11-17 12:58:45 +0100

    Factorize code of DH out of the views.

--- dj_dh/views.py
+++ dj_dh/views.py
@@ -6,12 +6,15 @@ import random
 
 def index(request):
     """Homepage"""
+
+    private_key, A = start_dh()
+
     # the session_key is used as key to retrieve the secret exponent
     session_key = random.getrandbits(256)
-    private_key = random.getrandbits(256)
     cache.set(session_key, private_key)
-    A = pow(settings.G, private_key, settings.P)
+
     print "session_key = %s\nprivate_key = %s\nA = %s" % (session_key, private_key, A)
+
     return render_to_response(
         'index.html', {
             'p': settings.P,
@@ -20,14 +23,32 @@ def index(request):
             'session_key': session_key,
     })
 
-def finish_dh(request):
+def start_dh():
+    """Generate the parameters needed to start a DH session.
+
+    Returns the private key and the A parameter to send to other user"""
+    private_key = random.getrandbits(256)
+    A = pow(settings.G, private_key, settings.P)
+
+    return private_key, A
+
+def finish_dh(private_key, B):
+    """Calculate the session key generated at the end of the DH exchange"""
+    K = pow(B, private_key, settings.P)
+
+    return K
+
+def finish(request):
     """Get B and compute K"""
     B = long(request.POST['B'])
     ciphertext = request.POST['password']
     session_key = request.POST['session_key']
     private_key = cache.get(session_key)
-    K = pow(B, private_key, settings.P)
+
+    K = finish_dh(private_key, B)
+
     print "password = %s\nsession_key = %s\nprivate_key = %s\nB = %s\nK = %s" % (ciphertext, session_key, private_key, B, K)
+
     return render_to_response(
         'finished.html', {
             'K': K,
